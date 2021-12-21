43068d6a6f6b56510543921b34252aa63954d092
Dima <kirabo007@gmail.com>
2021-12-21 14:38:50 +0300

    Initial commit

---
+++ src/UserType.php
@@ -0,0 +1,40 @@
+
+<?php
+namespace Bd\Type;
+require_once 'Bd/DB.php';
+require_once 'Bd/Types.php';
+
+use Bd\DB;
+use Bd\Types;
+use GraphQL\Type\Definition\ObjectType;
+
+class UserType extends ObjectType
+{
+
+public function ＿construct()
+{
+
+$config = [
+'description' => 'Пользователь',
+'fields' => function() {
+return [
+'id' => [
+'type' => Types::string(),
+'description' => 'Идентификатор пользователя'
+],
+'name' => [
+'type' => Types:: string(),
+'description' => 'Имя пользователя'
+],
+'email' => [
+'type' => Types:: string(),
+'description' => 'E-mail пользователя'
+],
+];
+}
+];
+
+parent::＿construct($config);
+
+}
+}
